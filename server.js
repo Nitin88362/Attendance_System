c0b047ac3fe71ec36a191ffe02661036d6d64220
Nitin88362 <nitin88362@example.com>
2026-05-07 14:44:12 +0530

    fixed mysql ssl

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ MySQL Connected");
+  }
+});
+
 const app = express();
 
 // Middleware
